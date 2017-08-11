4bbdd7e899ae3c9036c135d5d82afec1d325db3b
The Azure Guy <theazureguy007@gmail.com>
2017-08-11 19:31:13 +0530

    Deploy Paas Solution using Powershell & ARM Template as Input

---
+++ Deploy Paas Solution using Powershell & ARM Template as Input/Deploy-TheAzureGuy-Paas-Solution.ps1
@@ -0,0 +1,19 @@
+﻿### Defining Deployment Variables  ######
+{
+$location ='Central India'
+$resourceGroupName = 'theazureguy-simple-paas-template'
+$resourceDeploymentName = 'theazureguy-simple-paas-template-deployment'
+$templatePath = $env:SystemDrive + '\' + 'ARM_Template_Design'
+$templateFile = 'theazureguy-simple-paas.json'
+$template = $templatePath + '\' +$templateFile
+}
+
+### Create Resource Group   ######
+{
+New-AzureRmResourceGroup -Name $resourceGroupName -Location $location -verbose -Force
+}
+
+#### Deploy Resource using created json file
+{
+New-AzureRmResourceGroupDeployment -Name $resourceDeploymentName -ResourceGroupName $resourceGroupName -TemplateFile $template -Verbose -Force
+}
